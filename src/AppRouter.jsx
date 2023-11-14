5e29361c58b979c5925c2ad8c5c9d070d70a5799
ignaciodz <ignaciodiazbr@gmail.com>
2023-11-14 18:18:44 -0500

    agregando router

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import {Navigation} from './components/Navigation'
+import { HomePage, PokemonPage, SearchPage } from './pages'
 
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <Routes>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path='pokemon/:id' element={<PokemonPage/>}/>
+        <Route path='search' element={<SearchPage/>}/>
+      </Route>
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
   )
 }
