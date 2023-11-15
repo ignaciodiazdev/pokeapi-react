a259cfd82d0a78772e369e6e7c7d331172959dbb
ignaciodz <ignaciodiazbr@gmail.com>
2023-11-15 02:58:17 -0500

    agregando PokemonContext y Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { AppRouter } from "./AppRouter"
+import { PokemonProvider } from "./context/PokemonProvider"
 
 function App() {
 
   return (
-    <>
+    <PokemonProvider>
       <AppRouter/>
-    </>
+    </PokemonProvider>
   )
 }
 
---
+++ src/context/PokemonProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {PokemonContext} from './PokemonContext'
+
+export const PokemonProvider = ({children}) => {
+  
+  return (
+    <PokemonContext.Provider value={{
+      numero: 0
+    }}>
+      {children}
+    </PokemonContext.Provider>
+  )
+}
